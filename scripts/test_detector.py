43887d40e0f86a4a144260b74c6b2059b57f1670
Marija <gvozdenovicmara@gmail.com>
2026-04-30 00:39:28 +0200

    Add visualization with bounding boxes and labels

--- scripts/test_detector.py
+++ scripts/test_detector.py
@@ -12,4 +12,8 @@ image = cv2.imread("scripts/test_image.jpg")
 results = test_detector.detect(image)
 
 for result in results:
-    print(result.bbox, result.confidence)
+    cv2.rectangle(image,(int(result.bbox[0]), int(result.bbox[1])), (int(result.bbox[2]),int(result.bbox[3])), (0,255,0),3)
+    image = cv2.putText(img = image, text = result.class_name, org=(int(result.bbox[0]), int(result.bbox[1])), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,255,0), thickness=3)
+
+cv2.imshow('Title', image)
+cv2.waitKey(0)
