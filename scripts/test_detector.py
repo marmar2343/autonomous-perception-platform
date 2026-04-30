32b2292df7cf078d2ac82eba047206f764cbcae6
Marija <gvozdenovicmara@gmail.com>
2026-04-30 12:31:19 +0200

    Add DepthEstimator and fix depth map visualization

--- scripts/test_detector.py
+++ scripts/test_detector.py
@@ -3,17 +3,23 @@ import sys
 sys.path.append(".")
 
 from src.detectors.detector import ObjectDetector
+from src.depth.depth_estimator import DepthEstimator
 
 
 
 test_detector = ObjectDetector( "yolov8n.pt")
+test_estimator = DepthEstimator("depth-anything/Depth-Anything-V2-Small-hf")
 image = cv2.imread("scripts/test_image.jpg")
+image_detected = image.copy()
 
-results = test_detector.detect(image)
+results = test_detector.detect(image_detected)
 
 for result in results:
-    cv2.rectangle(image,(int(result.bbox[0]), int(result.bbox[1])), (int(result.bbox[2]),int(result.bbox[3])), (0,255,0),3)
-    image = cv2.putText(img = image, text = result.class_name, org=(int(result.bbox[0]), int(result.bbox[1])), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,255,0), thickness=3)
+    cv2.rectangle(image_detected,(int(result.bbox[0]), int(result.bbox[1])), (int(result.bbox[2]),int(result.bbox[3])), (0,255,0),3)
+    image_detected = cv2.putText(img = image_detected, text = result.class_name, org=(int(result.bbox[0]), int(result.bbox[1])), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,255,0), thickness=3)
 
-cv2.imshow('Title', image)
+image_depth = test_estimator.estimate(image)
+
+cv2.imshow('Title', image_detected)
+cv2.imshow('Depth map',image_depth)
 cv2.waitKey(0)
--- src/depth/depth_estimator.py
+++ src/depth/depth_estimator.py
@@ -1,10 +1,12 @@
 import numpy as np
 from transformers import pipeline
+from PIL import Image
 
 class DepthEstimator:
     def __init__(self, model:str, device:str = "cuda"):
         self.pipe = pipeline(task="depth-estimation", model = model, device=device)
 
     def estimate(self, frame:np.ndarray) -> np.ndarray:
-        pil_image = self.pipe(frame)["depth"] # return value of pipe is pil image
+        pil_frame = Image.fromarray(frame)
+        pil_image = self.pipe(pil_frame)["depth"] # return value of pipe is pil image
         return np.array(pil_image)
