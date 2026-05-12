c6ad72cc3c058e49dfe3dbc803c6ea5e33f3e2ad
Marija <gvozdenovicmara@gmail.com>
2026-05-12 14:33:21 +0200

    Add zone detection with visual feedback and YAML config

--- scripts/video_pipeline.py
+++ scripts/video_pipeline.py
@@ -1,19 +1,29 @@
 import cv2
 import sys
 import time
+import yaml
+import numpy as np
+import supervision as sv
 sys.path.append(".")
 
 from src.detectors.detector import ObjectDetector
 from src.depth.depth_estimator import DepthEstimator
 from src.fusion.fusion import fuse
 from src.tracking.tracker import Tracker
+from src.tracking.zone_detector import ZoneDetector
 
 cap = cv2.VideoCapture("scripts/test_video.mp4")
+
+with open("configs/zones.yaml") as f:
+    config = yaml.safe_load(f)
 
 test_detector = ObjectDetector( "yolov8n.pt")
 test_estimator = DepthEstimator("depth-anything/Depth-Anything-V2-Small-hf")
 test_tracker = Tracker()
+test_zone_detector = ZoneDetector(np.array(config["zones"][0]["coordinates"]), config["zones"][0]["threshold"])
 frame_count = 0
+
+
 
 while True:
     start = time.time()
@@ -36,13 +46,24 @@ while True:
 
     detection3D = fuse(tracks, depth_map)
 
+    dangerous_zone_detection = test_zone_detector.detect_in_zone(detection3D)
+
     for result in detection3D:
-        cv2.rectangle(frame,(int(result.bbox[0]), int(result.bbox[1])), (int(result.bbox[2]),int(result.bbox[3])), (0,255,0),3)
-        frame = cv2.putText(img = frame, text = str(result.track_id) + " " + str(result.depth), org=(int(result.bbox[0]), int(result.bbox[1])), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=5, color=(0,255,0), thickness=3)
+        if result.track_id in dangerous_zone_detection:
+            cv2.rectangle(frame,(int(result.bbox[0]), int(result.bbox[1])), (int(result.bbox[2]),int(result.bbox[3])), (0,0,255),3)
+            frame = cv2.putText(img = frame, text = str(result.track_id) + " " + str(result.depth), org=(int(result.bbox[0]), int(result.bbox[1])), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=5, color=(0,0,255), thickness=3)
+        else:
+            cv2.rectangle(frame,(int(result.bbox[0]), int(result.bbox[1])), (int(result.bbox[2]),int(result.bbox[3])), (0,255,0),3)
+            frame = cv2.putText(img = frame, text = str(result.track_id) + " " + str(result.depth), org=(int(result.bbox[0]), int(result.bbox[1])), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=5, color=(0,255,0), thickness=3)
+
+
 
     fps = 1/ (time.time()-start)
     print(f"FPS: {fps:.1f}")
 
+    zone_points = np.array(config["zones"][0]["coordinates"])
+    cv2.polylines(frame, [zone_points], isClosed=True, color=(0, 255, 255), thickness=5)
+    
     # Display the frame
     frame = cv2.resize(frame, (480, 640))
     cv2.imshow("Video", frame)
---
+++ src/tracking/zone_detector.py
@@ -0,0 +1,30 @@
+import numpy as np
+from src.fusion.types import Detection3D
+import supervision as sv
+
+class ZoneDetector:
+    def __init__(self, polygon: np.ndarray, treshold: int = 30):
+        self.polygon_zone = sv.PolygonZone(polygon=polygon)
+        self.treshold = treshold
+        self.track_id = {}
+
+    def detect_in_zone(self, detections3D: list[Detection3D]) -> list[int]:
+        xyxy = np.array([d.bbox for d in detections3D])
+        tracker_ids = np.array([d.track_id for d in detections3D])
+        sv_detections = sv.Detections(xyxy=xyxy, tracker_id=tracker_ids)
+        is_detections_in_zone = self.polygon_zone.trigger(sv_detections)
+
+        in_zone = [tracker_ids[i] for i in range(len(sv_detections)) if is_detections_in_zone[i]]
+
+        for ids in tracker_ids:
+            if ids in in_zone:
+                self.track_id[ids] = self.track_id.get(ids, 0) + 1
+            else:
+                self.track_id[ids] = 0
+
+        dangerous_zone = []
+        for ids in self.track_id:
+            if self.track_id[ids]>=self.treshold:
+                dangerous_zone.append(ids)
+
+        return dangerous_zone
